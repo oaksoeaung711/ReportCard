8a49dc805b2dec117daa7ebe79507b63a1df10e7
oaksoeaung711 <oaksoeaung.dev@gmail.com>
2023-08-17 09:01:40 +0630

    17-Aug-23

---
+++ app/Http/Controllers/SignController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSignRequest;
+use App\Http\Requests\UpdateSignRequest;
+use App\Models\Sign;
+
+class SignController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSignRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Sign $sign)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Sign $sign)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSignRequest $request, Sign $sign)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Sign $sign)
+    {
+        //
+    }
+}
